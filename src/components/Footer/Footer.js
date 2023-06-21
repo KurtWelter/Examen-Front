35f3340cf68e737c8d5ff27990dda0e3ce3a5b45
KurtWelter <alfredsmb1@gmail.com>
2023-06-20 22:49:28 -0400

    footer add

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="d-flex justify-content-center bg-dark p-3 ">
+      Todos los Derechos reservados a su respectivo dueño ©
+    </footer>
+  );
+};
+
+export default Footer;
